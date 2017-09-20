825420f099b7f5cafb51293c996378ef2d405811
WSlatton <williamslatton11@gmail.com>
2017-09-20 17:48:45 -0500

    added mecanum mode

--- src/org/lasarobotics/hazybot/Mecanum.java
+++ src/org/lasarobotics/hazybot/Mecanum.java
@@ -1,4 +1,26 @@
 package org.lasarobotics.hazybot;
 
-public class Mecanum {
+import org.json.simple.JSONObject;
+
+public class Mecanum implements Mode {
+    Hardware hardware;
+
+    public Mecanum(Hardware hardware, JSONObject options) {
+        this.hardware = hardware;
+    }
+    public void teleopPeriodic() {
+        double drive_power = hardware.getInput("drive");
+        double strafe_power = hardware.getInput("strafe");
+        double turn_power = hardware.getInput("turn");
+
+        double left_front_power = drive_power + turn_power + strafe_power;
+        double left_back_power = drive_power + turn_power - strafe_power;
+        double right_front_power = drive_power - turn_power + strafe_power;
+        double right_back_power = drive_power - turn_power - strafe_power;
+
+        hardware.setOutput("left_front", left_front_power);
+        hardware.setOutput("left_back", left_back_power);
+        hardware.setOutput("right_front", right_front_power);
+        hardware.setOutput("right_back", right_back_power);
+    }
 }
